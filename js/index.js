be158d8f9ea1a3d92df3a0e88774aa6dd7943f88
zw123zw <zw18674069215@163.com>
2022-01-08 14:22:39 +0800

    正则，模糊匹配学习

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 (function () {
-  console.log("正则start123");
+  const re1 = /hello/;
+  console.log(re1.test("hellos"));
+
+  const re2 = /ab{2,5}c/g;
+  console.log(re2.test("abbc"));
+  console.log("abc abbc abbbc abbbbc abbbbbc abbbbbbc".match(re2));
+
+  const re3 = /a[123]b/;
+  console.log(re3.test("a2b"));
+  console.log("a0b a1b a2b a3b a4b");
 })();
